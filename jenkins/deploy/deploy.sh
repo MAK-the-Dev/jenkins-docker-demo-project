5e5f4c21133924c4c71914c855c977290c414b0e
Muhammad Ahmad Khan <ahmad.khan@systemsltd.com>
2019-12-02 15:33:39 +0500

    second commit

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,6 +5,6 @@ echo docker-demo-dotnetcore > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i ~/.ssh/productionServer /tmp/.auth azureprouser@23.99.131.221:/tmp/.auth
-scp -i ~/.ssh/productionServer ./jenkins/deploy/publish azureprouser@23.99.131.221:/tmp/publish
-ssh -i ~/.ssh/productionServer azureprouser@23.99.131.221 "/tmp/publish"
+scp -i ~/.ssh/for_jenkins_to_access_prod /tmp/.auth azureprouser@23.99.131.221:/tmp/.auth
+scp -i ~/.ssh/for_jenkins_to_access_prod ./jenkins/deploy/publish azureprouser@23.99.131.221:/tmp/publish
+ssh -i ~/.ssh/for_jenkins_to_access_prod azureprouser@23.99.131.221 "/tmp/publish"
