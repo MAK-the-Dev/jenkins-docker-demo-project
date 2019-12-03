2ead795b26a8bbaf100111cc6855d95d28d03672
Muhammad Ahmad Khan <ahmad.khan@systemsltd.com>
2019-12-03 14:05:11 +0500

    edited the deploy.sh file

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -7,4 +7,4 @@ echo $PASS >> /tmp/.auth
 
 scp -i /var/lib/jenkins/workspace/ssh/for_jenkins_to_access_prod /tmp/.auth azureprouser@52.165.219.83:/tmp/.auth
 scp -i /var/lib/jenkins/workspace/ssh/for_jenkins_to_access_prod ./jenkins/deploy/publish azureprouser@52.165.219.83:/tmp/publish
-ssh -i /var/lib/jenkins/workspace/ssh/for_jenkins_to_access_prod azureprouser@52.165.219.83 "/tmp/publish"
+ssh -i /var/lib/jenkins/workspace/ssh/for_jenkins_to_access_prod azureprouser@52.165.219.83 "chmod +x /tmp/publish | /tmp/publish"
