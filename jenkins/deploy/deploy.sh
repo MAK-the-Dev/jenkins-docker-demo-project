3f81ceeb7934cb8c5d11b52c041e39ca30674446
Muhammad Ahmad Khan <ahmad.khan@systemsltd.com>
2019-12-10 15:15:54 +0500

    edited the hostname in deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -9,6 +9,6 @@ echo $IMG_B >> /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /var/lib/jenkins/workspace/ssh/for_jenkins_to_access_prod /tmp/.auth azureprouser@52.165.219.83:/tmp/.auth
-scp -i /var/lib/jenkins/workspace/ssh/for_jenkins_to_access_prod ./jenkins/deploy/publish azureprouser@52.165.219.83:/tmp/publish
-ssh -i /var/lib/jenkins/workspace/ssh/for_jenkins_to_access_prod azureprouser@52.165.219.83 "chmod +x /tmp/publish && sh /tmp/publish"
+scp -i /var/lib/jenkins/workspace/ssh/for_jenkins_to_access_prod /tmp/.auth azureprouser@vmpro.centralus.cloudapp.azure.com:/tmp/.auth
+scp -i /var/lib/jenkins/workspace/ssh/for_jenkins_to_access_prod ./jenkins/deploy/publish azureprouser@vmpro.centralus.cloudapp.azure.com:/tmp/publish
+ssh -i /var/lib/jenkins/workspace/ssh/for_jenkins_to_access_prod azureprouser@vmpro.centralus.cloudapp.azure.com "chmod +x /tmp/publish && sh /tmp/publish"
--- jenkins/test/test.sh
+++ jenkins/test/test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 echo "***************************"
-echo "** Testing the code ***********"
+echo "********** Testing the code **********"
 echo "***************************"
+
 # WORKSPACE=/home/azureuser/jenkins-dir/workspace
 
 # docker run --rm  -v  $WORKSPACE/project-app/Blog:/app  -w /app dotnetcore:latest "$@"
 
 # docker run --rm  -v  $WORKSPACE/project-app/ClientApp:/app  -w /usr/src/app angular:latest "$@"
 
-echo "** Done Nothing **"
+echo "** Did Nothing **"
 echo "** Test commands are commented **"
